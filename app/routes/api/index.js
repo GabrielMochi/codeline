58e684410c33892ef8223946a738fc627f08ad08
Gabriel Mochi Ribeiro <gmohi56@icloud.com>
2019-08-18 06:15:32 -0300

    logout implemented

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,18 +1,21 @@
 const { Router } = require('express')
+const user = require('./user')
 const seller = require('./seller')
 const establishment = require('./establishment')
 const coupon = require('./coupon')
 const couponQRCode = require('./couponQRCode')
+const { version } = require('../../../package.json')
 
 const router = Router()
 
+router.use('/user', user)
 router.use('/seller', seller)
 router.use('/establishment', establishment)
 router.use('/coupon', coupon)
 router.use('/couponQRCode', couponQRCode)
 
 router.get('/', (req, res) => {
-  res.end('API v.0.0.1')
+  res.end(`API v.${ version }`)
 })
 
 module.exports = router
